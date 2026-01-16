f6d754a5b4f5117a11f1c13bbca7fee57bf89e10
Tumppi066 <83072683+Tumppi066@users.noreply.github.com>
2026-01-16 17:02:56 +0200

    Cleanup repository

--- tumppi066.fi/src/pages/dashboard/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function Performance() {
-    return (
-        <iframe src="https://home.tumppi066.fi/#Homepage" className="w-full h-full" />
-    );
-}
--- tumppi066.fi/src/pages/performance/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function Performance() {
-    return (
-        <iframe src="https://dash.tumppi066.fi" className="w-full h-full" />
-    );
-}
